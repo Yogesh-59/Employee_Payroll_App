284a3393f237bd778466ea8021d712483db3d0df
Deepraj Lodhi <deeprajlodhi59@gmail.com>
2025-02-25 12:52:46 +0530

    [Deepraj]:Added Lombok @Slf4j for logging and configured logging levels in application.properties

--- employeepayrollapp/src/main/java/com/bridgelabz/employeepayrollapp/service/EmployeeService.java
+++ employeepayrollapp/src/main/java/com/bridgelabz/employeepayrollapp/service/EmployeeService.java
@@ -2,37 +2,48 @@ package com.bridgelabz.employeepayrollapp.service;
 
 import com.bridgelabz.employeepayrollapp.model.EmployeeModel;
 import com.bridgelabz.employeepayrollapp.repository.EmployeeRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
-
+@Slf4j
 @Service
 public class EmployeeService {
     public final EmployeeRepository repository;
     public EmployeeService(EmployeeRepository repository) {
+
         this.repository = repository;
     }
     public EmployeeModel addEmployee(EmployeeModel employee) {
+        log.info("Adding new employee:{}",employee);
         return repository.save(employee);
     }
 
     public List<EmployeeModel> getAllEmployees() {
+        log.info("Fetching all Employees");
         return repository.findAll();
     }
 
     public Optional<EmployeeModel> getEmployeeById(Long id) {
+       log.info("Fetching employee with ID: {}",id);
         return repository.findById(id);
     }
     public EmployeeModel updateEmployee(Long id, EmployeeModel updatedEmployee) {
+      log.info("Fetching employee with ID: {}",id);
         return repository.findById(id).map(employee -> {
             employee.setName(updatedEmployee.getName());
             employee.setDepartment(updatedEmployee.getDepartment());
             employee.setSalary(updatedEmployee.getSalary());
+            log.info("Updated employee details: {}",employee);
             return repository.save(employee);
-        }).orElseThrow(() -> new RuntimeException("Employee not found"));
+        }).orElseThrow(() -> {
+            log.error("Employee with ID {} not found",id);
+            return new RuntimeException("Employee not found");
+        });
     }
     public void deleteEmployee(Long id) {
+        log.info("Deleting employee with ID: {}",id);
         repository.deleteById(id);
     }
 }
